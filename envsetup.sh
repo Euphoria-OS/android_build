f714df8c3b72ab713108eff95eb0bf299d3dd642
Altaf-Mahdi <altaf.mahdi@gmail.com>
2014-11-30 23:30:50 +0000

    Remove custom bash_completion and repopick

--- envsetup.sh
+++ envsetup.sh
@@ -1682,7 +1682,7 @@ unset f
 
 # Add completions
 check_bash_version && {
-    dirs="sdk/bash_completion vendor/eos/bash_completion"
+    dirs="sdk/bash_completion"
     for dir in $dirs; do
     if [ -d ${dir} ]; then
         for f in `/bin/ls ${dir}/[a-z]*.bash 2> /dev/null`; do
