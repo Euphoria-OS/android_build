465dcfcc8ea38744dc0dc422ef5119c1967b9dc1
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-03-01 22:14:20 +0000

    Build Email and Exchange2
    Change-Id: Id8c194a6acf5e93b028cec3290eb8396b7e8be4b

--- target/product/core.mk
+++ target/product/core.mk
@@ -31,6 +31,8 @@ PRODUCT_PACKAGES += \
     DeskClock \
     DocumentsUI \
     DownloadProviderUi \
+    Email \
+    Exchange2 \
     ExternalStorageProvider \
     FusedLocation \
     InputDevices \
