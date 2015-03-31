8740588578d5307fb7de4fcbe53ef4cb92ff044e
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-03-31 16:19:29 +0100

    Revert "Revert "Build Email and Exchange2""
    This reverts commit 3c97f142b6d5c65c2d76ceb42ca82a6bbadcdef4.

--- target/product/core.mk
+++ target/product/core.mk
@@ -32,6 +32,8 @@ PRODUCT_PACKAGES += \
     DeskClock \
     DocumentsUI \
     DownloadProviderUi \
+    Email \
+    Exchange2 \
     ExternalStorageProvider \
     FusedLocation \
     InputDevices \
